8de9623a03b3cc1ea4b544ccacc22066e02c9f4b
godjooyoung <xolovesjoo36@naver.com>
2023-07-08 02:55:02 +0900

    Feat : about 내용

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,12 +5,18 @@ function About() {
     return (
         <AboutWrap>
             <AboutTitle>ABOUT</AboutTitle>
-            <AboutSubTitle>프론트앤드 개발자 신주영에 대해 소개</AboutSubTitle>
             <AboutContentWrap>
-                <h3>프론트앤드 개발자 신주영입니다.</h3>
-                <p>함께 일하고 싶은 개발자가 되고자 합니다.</p>
-                <p>서로 지식을 공유하고 성장하는 개발자 문화를 좋아합니다.</p>
-                <p>오래 살아남는 코드를 추구하며 서비스를 만드는 생산자라는 자부심으로 일합니다.</p>
+                <div>
+                    <h3>프론트앤드 개발자</h3>
+                    <span>신주영</span>
+                </div>
+                <p>
+                함께 일하고 싶은 개발자가 되고자 합니다.<br/>
+                서로 지식을 공유하고 성장하는 개발자 문화를 좋아합니다.<br/>
+                오래 살아남는 코드를 추구하며 서비스를 만드는 생산자라는 자부심으로 일합니다.<br/>
+                사용자가 제가 만든 서비스를 사용할 때 기쁨을 느낍니다.<br/>
+                </p>
+
             </AboutContentWrap>
         </AboutWrap>
     );
@@ -36,10 +42,35 @@ export const AboutSubTitle = styled.h2`
     text-align: center;
 `
 export const AboutContentWrap = styled.div`
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+    align-items: center;
+    margin-top: 5vh;
     font-size: 36px;
     font-weight: 400;
     h3{
         color: var(--po-00);
+        font-weight: 900;
     }
+    span{
+        font-size: 48px;
+        font-weight: 700;
+    }
+    div{
+        display: flex;
+        align-items: end;
+        gap: 10px;
+    }
+    p{
+        font-size: 18px;
+        font-weight: 400;
+        margin-top: 1rem;
+        margin-left: 1rem;
+        margin-right: 1rem;
+        line-height: 2rem;
+    }
+
+
 `
 export default About;
