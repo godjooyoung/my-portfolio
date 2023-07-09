e624b72739954547ffde70edca9345b9f4be0f1a
godjooyoung <xolovesjoo36@naver.com>
2023-07-10 06:38:13 +0900

    Feat : 프로젝트 설명 추가

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,10 +1,94 @@
 import React from 'react';
 import { styled } from 'styled-components';
-function Projects(props) {
+function Projects() {
     return (
         <ProjectsWrap>
             <ProjectsTitle>PROJECTS</ProjectsTitle>
-            <ProjectsSubTitle>프로젝트</ProjectsSubTitle>
+            <PjtsWrap>
+                <PjtItemWrap>
+                    <PjtItemInfo>
+                        <h2>모각코 ON:</h2>
+                        <h3>2023.07. (팀프로젝트)</h3>
+                    </PjtItemInfo>
+                    <PjtItemDescWrap>
+                        <PjtThumnailWrap>
+                            <PjtThumnailImg src={`${process.env.PUBLIC_URL}/image/pjt/_pjt_thumnail_mo.png`} alt="프로젝트이미지" />
+                        </PjtThumnailWrap>
+                        <PjtItemDesc>
+                            <PjtItemDescContent>
+                                <p>
+                                    코드리뷰를 하며 함께 성장할 수 있는 개발자들의 문화인 '모여서 각자 코딩' (이하, 모각코)을 위한 <span>위치기반 화상 채팅 서비스</span>입니다.
+                                    N:M 화상채팅, 화면공유, 실시간 채팅, 코드에디터, 위치기반 모각코 참여, 실시간 알림 기능, 통계데이터 시각화 등을 제공합니다.<br/>
+                                    <span>실제 서비스를 운영</span>하며 사용자 피드백을 반영하여 튜토리얼, 공지사항, 서비스 안정화, 쪽지기능 추가 및 강화를 통해<span>서비스 개선</span>을 하였습니다.<br/>
+                                    웹페이지 최적화를 위해 이미지 최적화, 폰트 최적화를 거쳐 <span>라이트하우스</span> 퍼포먼스 점수를 향상시켰습니다.
+                                </p>
+                                <ul>
+                                    <PjtItemListItem>
+                                        <p><span>FE - React</span></p>
+                                    </PjtItemListItem>
+                                    <PjtItemListItem>
+                                        <p><span>BE - Spring</span></p>
+                                    </PjtItemListItem>
+                                    <PjtItemListItem>
+                                        <p><span>주요기능</span></p>
+                                        <ul>
+                                            <li>
+                                                N:M 모각코 화상채팅, 화면공유
+                                            </li>
+                                            <li>
+                                                실시간 채팅, 실시간 알림
+                                            </li>
+                                            <li>
+                                                모각코 시간 통계 데이터 시각화
+                                            </li>
+                                            <li>
+                                                인터렉티브 사용자 튜토리얼
+                                            </li>
+                                        </ul>
+                                    </PjtItemListItem>
+                                    <PjtItemListItem>
+                                        <span>Deployment - Vercel</span>
+                                    </PjtItemListItem>
+                                    
+                                </ul>
+                            </PjtItemDescContent>
+                            <PjtItemDescBtnsWrap>
+                                <PjtItemDescBtn>GitHub</PjtItemDescBtn>
+                                <PjtItemDescBtn>Site</PjtItemDescBtn>
+                            </PjtItemDescBtnsWrap>
+                        </PjtItemDesc>
+                    </PjtItemDescWrap>
+                </PjtItemWrap>
+                <PjtItemWrap>
+                    <h2>프로젝트명</h2>
+                    <div>
+                        <div>이미지</div>
+                        <div>
+                            <div>
+                                <p>프로젝트 간단 설명 이런 절너 프로제트겨다 좋았다 어저고저저쪽</p>
+                                <ul>
+                                    <li>
+                                        <p>프로젝트기간 : 3.3.2.3.23.</p>
+                                    </li>
+                                    <li>
+                                        <p>be : 3.3.2.3.23.</p>
+                                    </li>
+                                    <li>
+                                        <p>fe : 3.3.2.3.23.</p>
+                                    </li>
+                                    <li>
+                                        <p>sdf : 3.3.2.3.23.</p>
+                                    </li>
+                                </ul>
+                            </div>
+                            <div>
+                                <button>버튼1</button><button>버튼2</button>
+                            </div>
+                        </div>
+                    </div>
+                </PjtItemWrap>
+            </PjtsWrap>
+
         </ProjectsWrap>
     );
 }
@@ -14,6 +98,7 @@ export const ProjectsWrap = styled.div`
     flex-direction: column;
     height: calc(100vh - 54px);
     background-color: var(--bg-00);
+    gap: 1rem;
 `
 export const ProjectsTitle = styled.h1`
     margin-top: 5vh;
@@ -25,5 +110,93 @@ export const ProjectsSubTitle = styled.h2`
     font-size: 40px;
     font-weight: 700;
     text-align: center;
+`
+
+// 프로젝트들
+export const PjtsWrap = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    row-gap: 20px;
+`
+// 프로젝트 아이템
+export const PjtItemWrap = styled.div`
+    width: 80%;
+    background-color: #30363D;
+    border-radius: 6px;
+    border: 0.75px solid #6E7681;
+    padding: 20px;
+`
+export const PjtItemInfo = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 1rem;
+    margin-bottom: 1rem;
+    h2{
+        font-size: 48px;
+        font-weight: 700;
+    }
+    h3{
+        font-size: 18px;
+        font-weight: 500;
+    }
+
+`
+export const PjtItemDescWrap = styled.div`
+    display: grid;
+    gap: 10px;
+    grid-template-columns: 1fr 1fr;
+    padding: 20px;
+`
+export const PjtThumnailWrap = styled.div`
+    width: 80%;
+    border-radius: 6px;
+    border: 0.75px solid #6E7681;
+    overflow: hidden;
+    margin: 0 auto;
+`
+export const PjtThumnailImg = styled.img`
+    object-fit: cover;
+    height: 100%;
+    width: 100%;
+`
+export const PjtItemDesc = styled.div`
+    font-size: 18px;
+    line-height: 1.6rem;
+    p{
+        color: #EBF5FF80
+    }
+    span {
+        color: #ffffff;
+    }
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+    justify-content: space-between;
+`
+
+export const PjtItemDescContent = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+`
+
+export const PjtItemDescBtnsWrap = styled.div`
+    text-align: right;
+`
+export const PjtItemDescBtn = styled.button`
+    width: 100px;
+    height: 30px;
+`
+
+export const PjtItemListItem = styled.li`
+    list-style: disc;
+    li{
+        list-style: circle;
+        list-style-position: inside;
+        color: #EBF5FF80
+    }
 `
 export default Projects;
--- src/components/Skill.jsx
+++ src/components/Skill.jsx
@@ -5,40 +5,139 @@ function Skill() {
     return (
         <SkillWrap>
             <SkillTitle>SKILL</SkillTitle>
-            <SkillPartWrap>
-                <SkillPart>
-                    <SkillItem>
-                        <SkillImg src={`${process.env.PUBLIC_URL}/image/skills/_skill_html.png`} alt="html아이콘"/>
-                        <SkillDesc>HTML설명</SkillDesc>
-                    </SkillItem>
-                    <SkillItem>
-                        <SkillImg src={`${process.env.PUBLIC_URL}/image/skills/_skill_css.png`} alt="css아이콘" />
-                        <SkillDesc>css설명</SkillDesc>
-                    </SkillItem>
-                    <SkillItem>
-                        <SkillImg src={`${process.env.PUBLIC_URL}/image/skills/_skill_js.png`} alt="js아이콘" />
-                        <SkillDesc>ES6 문법에 익숙합니다.</SkillDesc>
-                    </SkillItem>
-                    <SkillItem>
-                        <SkillImg src={`${process.env.PUBLIC_URL}/image/skills/_skill_react.png`} alt="react아이콘" />
-                        <SkillDesc>react 설명</SkillDesc>
-                    </SkillItem>
-                    <SkillItem>
-                        <SkillImg src={`${process.env.PUBLIC_URL}/image/skills/_skill_sc.png`} alt="sc아이콘" />
-                        <SkillDesc>스타일드 컴포넌트 설명</SkillDesc>
-                    </SkillItem>
-                </SkillPart>
-                <SkillPart>
-                    <SkillImg src={`${process.env.PUBLIC_URL}/image/skills/_skill_spring.png`} alt="spring아이콘" />
-                    <SkillImg src={`${process.env.PUBLIC_URL}/image/skills/_skill_spring.png`} alt="oracle아이콘" />
-                </SkillPart>
-                <SkillPart>
-                    <SkillImg src={`${process.env.PUBLIC_URL}/image/skills/_skill_vercel.png`} alt="vercel아이콘" />
-                    <SkillImg src={`${process.env.PUBLIC_URL}/image/skills/_skill_figma.png`} alt="figma아이콘" />
-                    <SkillImg src={`${process.env.PUBLIC_URL}/image/skills/_skill_git.png`} alt="git아이콘" />
-                    <SkillImg src={`${process.env.PUBLIC_URL}/image/skills/_skill_github.png`} alt="github아이콘" />
-                </SkillPart>
-            </SkillPartWrap>
+            <SkillsWrap>
+                <SkillPartWrap>
+                    <SkillPart>
+                        <SkillItem>
+                            <SkillImg src={`${process.env.PUBLIC_URL}/image/skills/_skill_html.png`} alt="html아이콘" />
+                            <SkillDesc>
+                                <SkillDescTitle>
+                                    HTML
+                                </SkillDescTitle>
+                                <SkillDescDetails>
+                                    웹표준을 준수합니다.
+                                </SkillDescDetails>
+                            </SkillDesc>
+                        </SkillItem>
+                        <SkillItem>
+                            <SkillImg src={`${process.env.PUBLIC_URL}/image/skills/_skill_css.png`} alt="css아이콘" />
+                            <SkillDesc>
+                                <SkillDescTitle>
+                                    CSS
+                                </SkillDescTitle>
+                                <SkillDescDetails>
+                                    <p>애니메이션</p>
+                                    <p>크로스브라우징</p>
+                                </SkillDescDetails>
+                            </SkillDesc>
+                        </SkillItem>
+                        <SkillItem>
+                            <SkillImg src={`${process.env.PUBLIC_URL}/image/skills/_skill_js.png`} alt="js아이콘" />
+                            <SkillDesc>
+                                <SkillDescTitle>
+                                    JAVASCRIPT
+                                </SkillDescTitle>
+                                <SkillDescDetails>
+                                    <p>ES6문법에 익숙합니다.</p>
+                                </SkillDescDetails>
+                            </SkillDesc>
+                        </SkillItem>
+                        <SkillItem>
+                            <SkillImg src={`${process.env.PUBLIC_URL}/image/skills/_skill_react.png`} alt="react아이콘" />
+                            <SkillDesc>
+                                <SkillDescTitle>
+                                    REACT
+                                </SkillDescTitle>
+                                <SkillDescDetails>
+                                    <p>컴포넌트 최적화 및 재사용에 관심이 많습니다.</p>
+                                </SkillDescDetails>
+                            </SkillDesc>
+                        </SkillItem>
+                        <SkillItem>
+                            <SkillImg src={`${process.env.PUBLIC_URL}/image/skills/_skill_sc.png`} alt="sc아이콘" />
+                            <SkillDesc>
+                                <SkillDescTitle>
+                                    Styled-Components
+                                </SkillDescTitle>
+                                <SkillDescDetails>
+                                    <p>props를 통한 동적 스타일링에 관심이 많습니다.</p>
+                                </SkillDescDetails>
+                            </SkillDesc>
+                        </SkillItem>
+                    </SkillPart>
+                    <SkillPart>
+                        <SkillItem>
+                            <SkillImg src={`${process.env.PUBLIC_URL}/image/skills/_skill_spring.png`} alt="spring아이콘" />
+                            <SkillDesc>
+                                <SkillDescTitle>
+                                    SPRING
+                                </SkillDescTitle>
+                                <SkillDescDetails>
+                                    <p>Spring, SpringBoot 앤드포인트에 대해 이해하고 있습니다.</p>
+                                </SkillDescDetails>
+                            </SkillDesc>
+                        </SkillItem>
+                        <SkillItem>
+                            <SkillImg src={`${process.env.PUBLIC_URL}/image/skills/_skill_spring.png`} alt="oracle아이콘" />
+                            <SkillDesc>
+                                <SkillDescTitle>
+                                    ORACLE
+                                </SkillDescTitle>
+                                <SkillDescDetails>
+                                    <p>오라클 전문 기관에서 ORACLE DBMS에 대해 배웠으며 SQLD 자격증 취득을 통해 쿼리에 대한 이해를 가지고 있습니다.</p>
+                                </SkillDescDetails>
+                            </SkillDesc>
+                        </SkillItem>
+                    </SkillPart>
+                    <SkillPart>
+                        <SkillItem>
+                            <SkillImg src={`${process.env.PUBLIC_URL}/image/skills/_skill_vercel.png`} alt="vercel아이콘" />
+                            <SkillDesc>
+                                <SkillDescTitle>
+                                    VERCEL
+                                </SkillDescTitle>
+                                <SkillDescDetails>
+                                    <p>Vercel을 통한 정적페이지 배포 경험이 있습니다.</p>
+                                </SkillDescDetails>
+                            </SkillDesc>
+                        </SkillItem>
+                        <SkillItem>
+                            <SkillImg src={`${process.env.PUBLIC_URL}/image/skills/_skill_figma.png`} alt="figma아이콘" />
+                            <SkillDesc>
+                                <SkillDescTitle>
+                                    FIGMA
+                                </SkillDescTitle>
+                                <SkillDescDetails>
+                                    <p>Figma를 통한 디자이너와 협업이 가능합니다.</p>
+                                </SkillDescDetails>
+                            </SkillDesc>
+                        </SkillItem>
+                        <SkillItem>
+                            <SkillImg src={`${process.env.PUBLIC_URL}/image/skills/_skill_git.png`} alt="git아이콘" />
+                            <SkillDesc>
+                                <SkillDescTitle>
+                                    GIT
+                                </SkillDescTitle>
+                                <SkillDescDetails>
+                                    <p>형상관리도구인 GIT을 이해하며 업무에서 충분히 다룰줄 압니다.</p>
+                                </SkillDescDetails>
+                            </SkillDesc>
+                        </SkillItem>
+                        <SkillItem>
+                            <SkillImg src={`${process.env.PUBLIC_URL}/image/skills/_skill_github.png`} alt="github아이콘" />
+                            <SkillDesc>
+                                <SkillDescTitle>
+                                    GITHUB
+                                </SkillDescTitle>
+                                <SkillDescDetails>
+                                    <p>GitHub를 통한 이슈관리 및 PullReqeust를 통해 효과적으로 협업합니다.</p>
+                                </SkillDescDetails>
+                            </SkillDesc>
+                        </SkillItem>
+                    </SkillPart>
+                </SkillPartWrap>
+
+            </SkillsWrap>
         </SkillWrap>
     );
 }
@@ -65,13 +164,22 @@ export const SkillSubTitle = styled.h2`
 export const SkillPartWrap = styled.div`
     display: flex;
     flex-direction: column;
-    gap: 10px;
-    width: 90%;
+    gap: 15px;
+    width: 66%;
+    margin: 0 auto;
+    flex: 1;
+`
+
+export const SkillsWrap = styled.div`
+    flex: 1;
+    display: flex;
+    justify-items: center;
+    align-items: center;
+    width: 66%;
     margin: 0 auto;
 `
 
 export const SkillPart = styled.div`
-    background-color: var(--bg-00);
     display: flex;
     display: grid;
     row-gap: 10px;
@@ -83,15 +191,32 @@ export const SkillDesc = styled.div`
     height: 190px;
     opacity: 0;
     background-color: rgba(0, 0, 0, 0.8);
-    border-radius: 15px;
+    border-radius: 14px;
     position: absolute;
     top: 0px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    gap: 5px;
 `
+export const SkillDescTitle = styled.h3`
+    font-size: 30px;
+    font-weight: 700;
+    margin: 0 1rem 0 1rem;
+`
+export const SkillDescDetails = styled.div`
+    margin: 0 1rem 0 1rem;
+    text-align: center;
+`
+
 export const SkillImg = styled.img`
     width: 190px;
     height: 190px;
 `
 export const SkillItem = styled.div`
+    display: flex;
+    justify-content: center;
     position: relative;
     &:hover ${SkillDesc} {
         opacity: 1;
